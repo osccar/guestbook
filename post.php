cb0655cfb3d323fc6a30278073b5c17e07fda579
Oscar Carrero <osccar@gmail.com>
2012-01-06 14:46:05 +0100

    code optimization
    Added comments, suggestions and TODO in read.php and post.php.
    Also separated some logic from data and added error messages

--- post.php
+++ post.php
@@ -1,52 +1,87 @@
 <?php
-    // VARS:
+    /**
+     * POST
+     *
+     * This file has two purposes:
+     *      - post a new comment
+     *      - edit a selected comment
+     *
+     *  TODO: it has basic input filtering and sanitization but a more curated filtering should be done
+     *      in order to make it more robust.
+     *
+     */
+    
     $unacceptable = array('fuck', 'ass', 'shit', 'asshole', 'jerk');
     $pepper = 'guestbk';
     $salt = '%';
-
-    // Check and sanitize input vars
+    $insert_info = '';
+    $error_msg = '';
+    $error_found = 0;   // 0 = No, 1 = Yes
+    
+    
+    // Sanitize and filter input vars (GET & POST)
+    
     if ( filter_has_var(INPUT_GET, 'op') )
-        {
-        @$option = filter_input( INPUT_GET, 'op', FILTER_SANITIZE_STRING );
-        }
+        $option = @filter_input( INPUT_GET, 'op', FILTER_SANITIZE_STRING );
+        
     if ( filter_has_var(INPUT_GET, 'entry') )
+        $entry_id = (int) @filter_input( INPUT_GET, 'entry', FILTER_SANITIZE_STRING );
+        
+    if ( @filter_has_var(INPUT_POST, 'GuestID') )
+        $GuestID = (int) @filter_input( INPUT_POST, 'GuestID', FILTER_SANITIZE_STRING );
+    
+    /**
+     * Basic requirements for all new comments:
+     *  - all fields must be filled
+     *  - all fields must be sanitized before inserting to DB
+     *
+     */
+    // Check all POST fields
+    if ( isset($_POST) )
         {
-        @$entry_id = filter_input( INPUT_GET, 'entry', FILTER_SANITIZE_INT );
-        }
-    if ( filter_has_var(INPUT_POST, 'GuestID') )
-        {
-        @$GuestID = filter_input( INPUT_POST, 'GuestID', FILTER_SANITIZE_INT );
-        }
-
-    // PROCESS COMMENTS
-
-    // Delete
-    if ( isset($option) && $option=='del' && isset($entry_id) )
-        {
-        require "connect.php";
-        $sql = "DELETE FROM guestbook WHERE id=$entry_id";
-        if ( $dbh->exec($sql) )
+        foreach ( $_POST as $field => $value )
             {
-            unset($dbh);
-            exit("Your comment has been deleted.<br>Click <a href='read.php'>here</a> return to the guestbook.");
+            // Min. is 1 character
+            if ( isset($_POST[$field]) && strlen(trim($value)) < 1 )
+                {
+                $error_msg = 'Please fill in all fields.';
+                $error_found = 1;
+                }
+            // Minimal email validation. Can be improved!
+            if ( $field === 'GuestEmail' )
+                if ( filter_var($value, FILTER_VALIDATE_EMAIL) === FALSE ) $error_found = 1;
             }
         }
+    
+    // Clean input data
+    $GuestName = addslashes(strip_tags(trim( $_POST['GuestName'] )));
+    $GuestPass = addslashes(strip_tags(trim( $_POST['GuestPass'] . $salt . $pepper )));
+    $GuestEmail = addslashes(strip_tags(trim( $_POST['GuestEmail'] )));
+    $GuestMessage = addslashes(strip_tags(trim( $_POST['GuestMessage'] )));
+    
+    
+    /**
+     * TODO: in-depth filtering should be added for robustness
+     *
+     * Add below...
+     *  
+     */
+
+
+
+    /** PROCESS COMMENTS ***********************************/
 
     // Edited message but no password provided
-    if ( isset($GuestID) && $_POST['GuestPass'] == '' )
-        {
+    if ( !isset($GuestID) && !isset($GuestPass) )
         exit("Please enter you password in order to edit the message. Try <a href='read.php'>again</a>.");
-        }
 
     // Edit
-    if ( isset($GuestID) && isset($_POST['GuestPass']) )
+    if ( isset($GuestID) && isset($GuestPass) )
         {
         require "connect.php";
 
         $GuestPass = addslashes(strip_tags(rtrim( $_POST['GuestPass'] . $salt . $pepper )));
-
         $GuestMessage = addslashes(strip_tags(rtrim( $_POST['GuestMessage'] )));
-        $GuestMessage = str_ireplace($unacceptable, "***", $GuestMessage);  // Remove unacceptable words
 
         // Verify password
         if ( $stmt = $dbh->query("SELECT COUNT(id) FROM guestbook WHERE guest_pass = PASSWORD('$GuestPass')") )
@@ -58,65 +93,51 @@
                         WHERE id = $GuestID";
 
                 if ( $dbh->exec($sql) )
-                    {
                     exit("Your comment has been successfully updated. <a href='read.php'>View</a> all guestbook messages.");
-                    }
                 else
-                    {
                     exit("Something went wrong. Please try again.");
-                    }
                 }
             else
-                {
                 exit("Invalid password. Try <a href='read.php'>again</a>.");
-                }
             }
          unset($_POST);
          unset($dbh);
         }
 
     // New comment or Edit comment
-    if ( !isset($option) && isset($_POST['GuestName']) && isset($_POST['GuestPass']) )
+    if ( !isset($option) && isset($GuestName) && isset($GuestPass) )
         {
         require "connect.php";
-
-        $GuestName = addslashes(strip_tags(rtrim( $_POST['GuestName'] )));
-        $GuestPass = addslashes(strip_tags(rtrim( $_POST['GuestPass'] . $salt . $pepper )));
-        $GuestEmail = addslashes(strip_tags(rtrim( $_POST['GuestEmail'] )));
-        $GuestMessage = addslashes(strip_tags(rtrim( $_POST['GuestMessage'] )));
-        $GuestMessage = str_ireplace($unacceptable, "***", $GuestMessage);  // Remove unacceptable words
 
         $sql = "INSERT INTO guestbook (guest_name, guest_pass, guest_email, guest_message)
                 VALUES ('$GuestName', PASSWORD('$GuestPass') ,'$GuestEmail', '$GuestMessage')";
 
-        // Insert comment or bust
+        // Insert comment or bust!
         if ( $stmt = $dbh->query($sql) )
-            {
-            exit("Thanks for leaving a comment. View the guestbook <a href='read.php'>here</a>.");
-            }
+            $insert_info = "Thanks for leaving a comment. View the guestbook <a href='read.php'>here</a>.";
         else
-            {
-            echo "There was an error adding your entry. Please try again.";
-            }
+            $insert_info = "There was an error adding your entry. Please try again.";
+
         unset($_POST);
         unset($dbh);
         }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang=es>
 <head>
+    <meta charset=utf-8>
     <title>Post comments in Guestbook</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h2>Please leave a comment (be gentle!)</h2>
+    <h2>Leave a comment (please be gentle!)</h2>
 
     <?php
     // Edit comment in guestbook
-    if ( isset($option) && $option=='edit' && isset($entry_id) )
+    if ( isset($option) && $option==='edit' && isset($entry_id) )
         {
         require "connect.php";
-
         if ( $stmt = $dbh->query("SELECT COUNT(*) FROM guestbook") )
             {
             $sql = "SELECT guest_name, guest_email, date_submitted
@@ -125,9 +146,6 @@
 
             $stmt = $dbh->query($sql);
             $entry = $stmt->fetchAll();
-            //echo '<pre>';
-            //var_dump($entry);
-
             $date_submitted = date("F j, Y g:i a", strtotime($entry[0]["date_submitted"]));
             print "
                 <p><strong>Previously submitted on $date_submitted</strong></p>
@@ -145,18 +163,32 @@
             print "<br><a href='read.php'>Read comments</a>";
             }
         }
-    else
-        // Post a new comment
+    // Alert errors
+    elseif ( $error_found )
         {
+        print '<div class=insert-info>'. $
+        
+        }
+    // Post a new comment
+    else
+        {
+        if ( !empty($insert_info) )
+            print "<div class=insert-info>$insert_info</div>";
     ?>
-        <form method='post' action='post.php'>
-            Name: <input type='text' name='GuestName'><br>
-            Pass: <input type='password' name='GuestPass'><br>
-            Email: <input type='text' name='GuestEmail'><br><br>
-            Message:<br><textarea rows='10' cols='40' name='GuestMessage'></textarea>
-            <br><br>
-            <input type='submit' value='Post comment'>
-        </form>
+        <div id=comments-form>
+            <form method=post action=post.php>
+                <p>Name</p>
+                <p><input type=text name=GuestName placeholder='John Doe'></p>
+                <p>Password</p>
+                <p><input type=password name=GuestPass placeholder='Make it good!'></p>
+                <p>Email</p>
+                <p><input type=email name=GuestEmail placeholder=name@domain.com></p>
+                <p>Comment</p>
+                <p><textarea rows=10 cols=60 name=GuestMessage></textarea></p>
+                <br>
+                <input type=submit class='button cyan' value='Leave a comment'>
+            </form>
+        </div>
     <?php } ?>
 </body>
 </html>
--- read.php
+++ read.php
@@ -1,156 +1,170 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Guestbook entries</title>
-    <style>
-        ul{
-            margin: 0 auto; 
-            width: 395px;
-            list-style-type: none;
-        }
-        li {
-            display:inline;
-        }
-        
-        li.selected {
-            text-decoration: none;
-            color: black;
-            font-weight: bold;
-            background-color: #c0c0c0;
-            padding: 10px;
-        }
-    </style>
-</head>
-<body>
-    <h2>Guestbook entries</h2>
-    <hr>
-
-    <?php
+<?php
+    /**
+     * READ
+     *
+     * This file has two purposes:
+     *      - read/show comments (using pagination)
+     *      - delete a selected comment
+     *
+     *  TODO: it has basic input filtering and sanitization but some further filtering should be done
+     *      in order to make it much more robust.
+     *
+     */
     error_reporting(E_ALL);
     session_start();    // For paginated results tracking
-    
+
     require "connect.php";
-    //require "DB.class.php";
     require "Pager.class.php";
-
-    //if ( $conn = new DB('test','root','AdminSQL') ){
-    //    $conn->getInstance();
-    //    exit;
-    //}
-
+    
     $page_url = htmlentities($_SERVER['PHP_SELF']); // avoid injection
     $limit = 5; // results per page
-    $range_options = array('min' => 1, 'max' => $_SESSION['total_records']);
+    $deleted = '';
+    $unacceptable = array('fuck', 'ass', 'shit', 'asshole', 'jerk');
     
+    // Sanitize and filter GET vars
+    if ( filter_has_var(INPUT_GET, 'entry') )
+        $entry_id = (int) @filter_input( INPUT_GET, 'entry', FILTER_SANITIZE_STRING );
+        
+    if ( filter_has_var(INPUT_GET, 'op') )
+        $option = @filter_input( INPUT_GET, 'op', FILTER_SANITIZE_STRING );
+
+    /**
+     * Basic requirements for all comments:
+     *  - all text from comment field must be checked for unacceptable words! (line 147)
+     *
+     * TODO: extra filtering can be added if needed (not required)
+     *
+     * Add below...
+     *  
+     */
+
+
+
+    // Delete comment
+    if ( isset($option) && $option==='del' && isset($entry_id) )
+        {
+        $sql = "DELETE FROM guestbook WHERE id = $entry_id";
+        if ( $dbh->exec($sql) )
+            $delete_info = "<p id=del-message>Your comment has been deleted.</p>";
+        else
+            $delete_info = "<p class=error>Sorry. Your comment could not be deleted</p>";
+        }
+?>
+
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <title>Simple Guestbook</title>
+    <!--[if gte IE 9]>
+        <style type="text/css">
+            .gradient { filter: none; }
+        </style>
+    <![endif]-->    
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Simple Guestbook</h2>
+    <hr>
+    <?php
     // Get total records from DB
     if ( ! isset($_SESSION['total_records']) )
-        {
         try {
             $rows = $dbh->query("SELECT COUNT(id) FROM guestbook");
             $_SESSION['total_records'] = $rows->fetchColumn();
-            }
+        }
         catch (Exception $e) {
             $_SESSION['total_records'] = 0;
-            }
         }
     else
         {
+        $range_options = array('min' => 1, 'max' => $_SESSION['total_records']);
+
         // Check page number for pagination
         if ( ! filter_has_var(INPUT_GET, 'page') )
-            {
-            $page = 1;  // no page was set    
-            }
+            $page = 1;  // no page was set
         // Check page value within limits
         elseif ( ! filter_var($_GET['page'], FILTER_VALIDATE_INT, $range_options) )
-            {
             $page = 1;  // validation was not possible
-            }
         else
-            {
             $page = (int)$_GET['page']; // sets the page
-            }    
-            
+
         // Build pagination links
         if ( $_SESSION['total_records'] == 0 )
-            {
             print "<em>Guestbook seems to be empty! :-( <br>Wouldn't you like to be the first? Go ahead, click <a href='post.php'>here!</a>";
             //$content = 'No records available';
-            }
         else
             {
-            // Prepare pagination vars
+            // Prepare pagination data
             $pager = Pager::getPagerData($_SESSION['total_records'], $limit, $page);
             $offset = $pager->offset;
             $limit = $pager->limit;
             $page = $pager->page;
-            $pagination = '<ul>';
-            
+
+            /** SHOW PAGINATION **********************/
+
+            $pagination = '<ul id=pager>';
+
             // If first page, no PREVIOUS link required
             if ( $page != 1 )
-                {
-                $pagination .= '<li><a href=' . $page_url . '?page=' . ($page - 1) . '>&lt;&lt; PREV </a></li>';
-                }
-            
+                $pagination .= '<li><a href=' . $page_url . '?page=' . ($page - 1) . '>&lt;&lt;</a></li>';
+
             // Create pagination
             for ( $i=1; $i <= $pager->num_pages; $i++ )
                 {
                 if ( $i == $pager->page ) // current page
-                    {
                     $pagination .= '<li class=selected>' . $i . '</li>';
-                    }
                 else
-                    {
                     $pagination .= '<li><a href=' . $page_url . '?page=' . $i . '>' . $i . '</a></li>' . "\n";
-                    }
                 }
-            
+
             // If last page, no NEXT link required
             if ( $page < $pager->num_pages )
-                {
-                $menu .= '<li><a href=' . $page_url . '?page=' . ($page + 1) . 'NEXT &gt;&gt;</a></li>';
-                }
-            
+                $pagination .= '<li><a href=' . $page_url . '?page=' . ($page + 1) . '>&gt;&gt;</a></li>';
+
             $pagination .= '</ul>';
+
+            /** COMMENTS ****************************/
+
+            // Retrieve records
+            $sql = "SELECT id, guest_name, guest_email, guest_message, date_submitted
+                    FROM guestbook
+                    ORDER BY date_submitted DESC
+                    LIMIT $limit
+                    OFFSET $offset";
+
+            print "<h5>Total entries in guestbook: {$_SESSION['total_records']} </h5>";
+            print "<div class=new-comment-btn><a class='button medium cyan' href='post.php'><span>New comment</span></a></div>";
             
-            // Retrieve records from DB
-            //if ( $stmt = $dbh->query("SELECT COUNT(*) FROM guestbook") )
-            //    {
-                print "<h5>Total entries in guestbook: {$_SESSION['total_records']} </h5>";
-                //$sql = "SELECT id, guest_name, guest_email, guest_message, date_submitted
-                //        FROM guestbook
-                //        ORDER BY date_submitted DESC";
-                $sql = "SELECT id, guest_name, guest_email, guest_message, date_submitted
-                        FROM guestbook
-                        LIMIT $limit 
-                        OFFSET $offset";
-    
-                print $pagination .'<br><br>';
-    
-                foreach ( $stmt = $dbh->query($sql) as $entry )
-                    {
-                    $date_submitted = date('F j, Y g:i a', strtotime($entry['date_submitted']));
-                    printf("<strong>Posted by <a href='mailto:%s'>%s</a> on %s</strong><br>",
-                        $entry['guest_email'],
-                        $entry['guest_name'],
-                        $date_submitted
-                    );
-                    print '<p><em>'. wordwrap($entry['guest_message'], 100, '<br>') .'</em></p>';
-                    printf("%s | %s",
-                        "<a href='post.php?op=edit&entry={$entry['id']}' title='Edit comment'>Edit</a>",
-                        "<a href='post.php?op=del&entry={$entry['id']}' title='Delete comment'>Delete</a>"
-                    );
-                    print "<p>-------------------------------------------------------------------------------</p>";
-                    }
-                
-                print "<br><a href='post.php'>Add new message</a>";
-            //    }
-            //else
-            //    {
-            //    print "<em>Guestbook seems to be empty! :-( <br>Wouldn't you like to be the first? Go ahead, click <a href='post.php'>here!</a>";
-            //    }            
+            // Show deleted comment information, in case user deleted
+            ( $delete_info ) ? print $delete_info : NULL;
+            
+            $delete_info = '';  // reset delete message
+            
+            // Show comments
+            print "<div id=gb-comments>";
+            foreach ( $stmt = $dbh->query($sql) as $entry )
+                {
+                // Clean message: unacceptable words
+                $guest_message = str_ireplace($unacceptable, "***", $entry['guest_message']);
+                $record = '<div class="gb-entry gradient">';
+                $date_submitted = date('F j, Y g:i a', strtotime($entry['date_submitted']));
+                $record .= sprintf("<h3><a class=guest-name href='mailto:%s'>%s</a></h3>", $entry['guest_email'], $entry['guest_name']);
+                $record .= '<p><em>'. $entry['guest_message'] .'</em></p>';
+                $record .= '<span>'. $date_submitted .'</span>';
+                $record .= sprintf("
+                    <div class=edit-del-btns>%s %s</div>",
+                    "<a class='button small green' href='post.php?op=edit&entry={$entry['id']}'><span>Edit</span></a>",
+                    "<a class='button small red' href='read.php?op=del&entry={$entry['id']}'><span>Delete</span></a>"
+                );
+                $record .= "</div>\n";
+                print $record;
+                }
+            print "</div>";
+            print $pagination .'<br>';
             }
         }
-
+    
     $dbh = null;
     ?>
 </body>
